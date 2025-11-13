bbbc19698e97219257cd55757d531e163122391d
Calvin Buckley <calvin@cmpct.info>
2025-11-13 09:46:55 -0500

    ASCII flag should only be used for read-only string args
    Check for this.

--- pgmfunc.hxx
+++ pgmfunc.hxx
@@ -9,12 +9,27 @@ extern "C" {
 }
 
 #include <stdexcept>
+#include <type_traits>
+#if defined(__cpp_lib_logical_traits)
+template<typename... Args>using Conjunction = std::conjunction<Args...>;
+#else
+#include <experimental/type_traits> // just assume for GCC 6, no polyfill
+template<typename... Args>using Conjunction = std::experimental::conjunction<Args...>;
+#endif
 
 /*
  * Type-safe compile-time wrapper for IBM i *PGM objects. The OPM calling
  * convention is pass-by-reference; this automatically passes value types
  * passed to the function into pointers to the stack.
  */
+
+/*
+ * Because _PGMCALL's ASCII mode takes only strings, and can't write back
+ * to them, we'll want to check to make sure you're passing constant
+ * strings to it. Maybe check for char* too?
+ */
+template<class What, class... Args>
+struct ParameterPackAllAre : Conjunction<std::is_same<What, Args>...> {};
 
 // If passed by value: get the reference on the stack (and de-constify)
 template<typename T> constexpr void *ParameterArrayMember(T &obj) { return static_cast<void*>(&obj); }
@@ -43,6 +58,11 @@ private:
 	constexpr int process_flags(int flags) {
 		if (sizeof...(TArgs) > PGMCALL_MAXARGS) {
 			flags |= PGMCALL_NOMAXARGS;
+		}
+		// Can't be a static assert here, flags is non-constexpr
+		if (ParameterPackAllAre<const char*, TArgs...>::value == false
+				 && (flags & PGMCALL_ASCII_STRINGS)) {
+			throw std::invalid_argument("all values must be const char* for ASCII strings flag");
 		}
 		// XXX: PGMCALL_DIRECT_ARGS requires different argv type
 		return flags & (PGMCALL_DROP_ADOPT
